68d8149a777c6b68027e437f499ad10568289332
krushnaCh <166533443+krushnaCh@users.noreply.github.com>
2024-04-16 19:46:40 +0000

    datatypes

---
+++ datatpes.js
@@ -0,0 +1,39 @@
+"use strict";
+
+
+
+
+
+
+
+
+
+
+
+//"use strict";//treat all js code as newer version .
+
+//alert(3+3)//we are using node js ,not browser
+//code readabilty should high 
+//MDN documentation refer 
+
+//number =>2 to power 53
+// bigint
+//string=""
+//boolean=true/false 
+
+//null=standalone value
+
+//indefined=>abhi value assign nahi  hai hai 
+
+//Symbol
+
+//Object 
+
+
+console.log(3+3);
+console.log("Krushna");
+
+console.log(typeof "krushna");//string
+console.log(typeof null);    //object 
+console.log(typeof undefined);  //undefined 
+
