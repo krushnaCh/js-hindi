526c052a110a0a811c96987c1bda7b14ec48cc9d
krushnaCh <166533443+krushnaCh@users.noreply.github.com>
2024-04-22 05:12:48 +0000

    container config done

---
+++ comaparision.js
@@ -0,0 +1,15 @@
+// let str1="Hello";
+// let str2="Krushna";
+// let str3=str1+str2;
+// console.log(str3)
+console.log(true);
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+console.log(undefined>0);
+console.log(undefined<0);
+console.log(undefined==0);
+
+console.log("2"==2);
+
