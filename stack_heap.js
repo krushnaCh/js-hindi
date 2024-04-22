313aeffc14ea315414b61b6dad205bd98ed6c822
krushnaCh <166533443+krushnaCh@users.noreply.github.com>
2024-04-22 10:09:20 +0000

    stack_heap

---
+++ stack_heap.js
@@ -0,0 +1,14 @@
+//heap memory(non primitive type ) and stack memory(primitive)
+
+let myYoutubename="Krushna_chavan"
+
+let anothername=myYoutubename
+
+console.log(anothername);
+
+
+let userOne={
+    name:"krushna"
+    email:"krushnachavan@gmail.com"
+
+}
